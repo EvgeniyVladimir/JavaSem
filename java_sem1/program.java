c75a9e196beec9e1b2bee0c6956d417411a20d85
Evgeniy_Vladimir <6484588@mail.ru>
2022-11-22 00:07:35 +0300

    java_sem1

---
+++ java_sem1/program.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class program {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String t = sc.nextLine();
+        System.out.println("Привет, " + t +" !");
+
+        
+    }
+}
