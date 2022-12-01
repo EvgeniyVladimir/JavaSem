9be184032ac57ad78734eb74b1044c40ab685d04
Evgeniy_Vladimir <6484588@mail.ru>
2022-12-01 20:53:16 +0300

    correction java_sem1

--- java_sem1/Program.java
+++ java_sem1/Program.java
@@ -91,35 +91,35 @@
 // Задача 4. Задан массив, например, nums = [1,7,3,6,5,6].
 // Написать программу, которая найдет индекс i для этого массива такой, что сумма элементов с индексами < i равна сумме элементов с индексами > i.
 
-package java_sem1;
+// package java_sem1;
 
-public class Program{
+// public class Program{
 
 
-    public static void main(String[] args) {
-        int[] numbers = {1, 7, 3, 6, 5, 6};
-        int median = findMedian(numbers);
-        System.out.println("median");
-    }
+//     public static void main(String[] args) {
+//         int[] numbers = {1, 7, 3, 6, 5, 6};
+//         int median = findMedian(numbers);
+//         System.out.println("median");
+//     }
     
-    private static int findMedian(int[] numbers) {
-    // [a1, a2, a3, a4, a5, a6, a7]
+//     private static int findMedian(int[] numbers) {
+//     // [a1, a2, a3, a4, a5, a6, a7]
     
-        int left = 0;
-        for (int i = 1; i < numbers.length - 1; i++) {
-            left += numbers[i - 1];
+//         int left = 0;
+//         for (int i = 1; i < numbers.length - 1; i++) {
+//             left += numbers[i - 1];
     
-            int right = 0;
-            // i + 1, i + 2, ... number.length - 1
-            for (int j = i + 1; j < numbers.length; j++) {
-                right += numbers[j];
-            }
+//             int right = 0;
+//             // i + 1, i + 2, ... number.length - 1
+//             for (int j = i + 1; j < numbers.length; j++) {
+//                 right += numbers[j];
+//             }
         
-            if (left == right) {
-                return i;
-            }
-        }
+//             if (left == right) {
+//                 return i;
+//             }
+//         }
     
-        return -1;
-    }
-}
+//         return -1;
+//     }
+// }
